cd3190a523ed13fefcbdf106b9e9f564dcdf1577
Holgerrson-A <75788046+Holgerrson-A@users.noreply.github.com>
2021-03-27 11:58:30 +0100

    Jag är klar med hemsidan nu gött!
    Hej

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function showMenu() {
+    document.querySelector(".nav").classList.toggle("show")
+    console.log("najs")
+}
+
+document.querySelector(".buttonimg").addEventListener("click", showMenu)
